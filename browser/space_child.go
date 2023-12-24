89ca6d13247ce7797b6e58b060040e4b7f619228
Andrew Arrow <oneone@gmail.com>
2023-12-24 12:21:57 -0800

    We need to index the bluetooth FTP program!

---
+++ browser/space_child.go
@@ -0,0 +1,15 @@
+package browser
+
+import "strings"
+
+func (s *Space) Child() {
+	buffer := []string{}
+	for i, line := range s.Lines {
+		buffer = append(buffer, line)
+		if i == s.CurrentLine {
+			buffer = append(buffer, "  "+line)
+		}
+	}
+	s.Markup = strings.Join(buffer, "\n")
+	s.Render()
+}
---
+++ browser/space_color.go
@@ -0,0 +1,19 @@
+package browser
+
+import "strings"
+
+func (s *Space) Color() {
+	buffer := []string{}
+	for i, line := range s.Lines {
+		if i == s.CurrentLine {
+			spaces := getSpaces(line)
+			m, tag := makeClassMap(line)
+			m["bg-r"] = true
+			buffer = append(buffer, spaces+tag+" "+makeClasses(m))
+			continue
+		}
+		buffer = append(buffer, line)
+	}
+	s.Markup = strings.Join(buffer, "\n")
+	s.Render()
+}
