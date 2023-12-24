61069856b48ef3dcf84227f9ce76373a45030a78
Andrew Arrow <oneone@gmail.com>
2023-12-24 07:23:01 -0800

    Indexing the matrix won't do anything, we need to connect the optical SSL matrix!

--- browser/space_render.go
+++ browser/space_render.go
@@ -1,5 +1,18 @@
 package browser
 
+import (
+	"fmt"
+	"strings"
+)
+
 func (s *Space) Render() {
-	s.Left.Set("innerHTML", s.Markup)
+	lines := strings.Split(s.Markup, "\n")
+	buffer := []string{}
+	for i, line := range lines {
+
+		p := fmt.Sprintf(`p id=line%d`+"\n", i+i)
+		s := fmt.Sprintf(strings.ReplaceAll(line, " ", "&nbsp;"))
+		buffer = append(buffer, p+s)
+	}
+	s.Left.Set("innerHTML", strings.Join(buffer, "\n"))
 }
