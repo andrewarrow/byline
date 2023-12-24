62c7ac8e5a2b869e6d2f7ec36272cb201ea68d8f
Andrew Arrow <oneone@gmail.com>
2023-12-24 07:36:13 -0800

    Indexing the card won't do anything, we need to transmit the wireless TCP feed!

--- browser/space.go
+++ browser/space.go
@@ -20,12 +20,12 @@ var space = Space{}
 
 func RegisterSpaceEvents() {
 	Document.Document.Call("addEventListener", "keydown", js.FuncOf(keyPress))
-	space.Markup = `div id=tag1 br-g w-64 h-64
-  div id=tag2 br-g w-9 h-9
-    div id=tag3 br-g w-9 h-9
-  div id=tag4 br-g w-9 h-9
-  div id=tag5 br-g w-9 h-9
-  div id=tag6 br-g w-9 h-9`
+	space.Markup = `div br-g w-64 h-64
+  div br-g w-9 h-9
+    div br-g w-9 h-9
+  div br-g w-9 h-9
+  div br-g w-9 h-9
+  div br-g w-9 h-9`
 	space.MaxLines = len(strings.Split(space.Markup, "\n"))
 	space.Left = Document.ByIdWrap("left")
 	space.Right = Document.ByIdWrap("right")
--- markup/space.mu
+++ markup/space.mu
@@ -2,9 +2,3 @@ div text-lg
   div flex
     div w-1/2 font-mono id=left
     div ml-3 w-1/2 id=right
-      div id=tag1 bg-r w-64 h-64
-        div id=tag2  br-g w-9 h-9
-          div id=tag3 bg-r w-9 h-9
-        div id=tag4 br-g w-9 h-9
-        div id=tag5 bg-r w-9 h-9
-        div id=tag6 br-g w-9 h-9
