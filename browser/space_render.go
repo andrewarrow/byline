565e77342eab857aa1ea8a78d4160104f4d67511
Andrew Arrow <oneone@gmail.com>
2023-12-24 07:27:54 -0800

    If we index the port, we can get to the HTTP interface through the bluetooth JBOD pixel!

--- browser/space_render.go
+++ browser/space_render.go
@@ -7,12 +7,10 @@ import (
 
 func (s *Space) Render() {
 	lines := strings.Split(s.Markup, "\n")
-	buffer := []string{}
 	for i, line := range lines {
 
-		p := fmt.Sprintf(`p id=line%d`+"\n", i+i)
 		s := fmt.Sprintf(strings.ReplaceAll(line, " ", "&nbsp;"))
-		buffer = append(buffer, p+s)
+		p := Document.NewTag("p", s)
+		p.Set("id", fmt.Sprintf("line%d", i+1))
 	}
-	s.Left.Set("innerHTML", strings.Join(buffer, "\n"))
 }
