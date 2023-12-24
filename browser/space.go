344f765d5d35c4e2cd012f364055c30e2d0ab990
Andrew Arrow <oneone@gmail.com>
2023-12-24 00:26:51 -0800

    Try to compress the IB alarm, maybe it will navigate the open-source monitor!

--- browser/space.go
+++ browser/space.go
@@ -1,7 +1,18 @@
 package browser
 
+import (
+	"fmt"
+	"syscall/js"
+)
+
 type Space struct {
 }
 
 func RegisterSpaceEvents() {
+	Document.Document.Call("addEventListener", "keydown", js.FuncOf(keyPress))
+}
+
+func keyPress(this js.Value, p []js.Value) any {
+	fmt.Println(p)
+	return nil
 }
