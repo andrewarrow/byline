1c8e862fa2c37bf3bb2e6443baa3df35007fe539
Andrew Arrow <oneone@gmail.com>
2023-12-24 00:13:51 -0800

    The JSON bus is down, reboot the online system so we can program the PCI capacitor!

--- browser/setup.go
+++ browser/setup.go
@@ -4,3 +4,11 @@ import "github.com/andrewarrow/feedback/wasm"
 
 var Global *wasm.Global
 var Document *wasm.Document
+
+func RegisterEvents() {
+	if Global.Start == "welcome.html" {
+		//RegisterLoginEvents()
+	} else if Global.Start == "space.html" {
+		RegisterSpaceEvents()
+	}
+}
