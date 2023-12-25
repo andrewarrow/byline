eb721d294cbfac80161f6d942b812acd95dad48f
Andrew Arrow <oneone@gmail.com>
2023-12-25 15:46:24 -0800

    Overriding the driver won't do anything, we need to synthesize the mobile AGP system!

--- browser/vim_insert.go
+++ browser/vim_insert.go
@@ -7,6 +7,9 @@ func (v *Vim) Insert(k string) {
 	if k == "ArrowUp" || k == "ArrowDown" || k == "ArrowLeft" || k == "ArrowRight" {
 		return
 	}
+	if k == "Enter" || k == "" || k == "" || k == "" {
+		return
+	}
 
 	s := v.Lines[v.Y]
 	prefix := s[0:v.X]
