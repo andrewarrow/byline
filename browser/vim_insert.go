a0e26aedf9c8e90622b822f3beef5708a81fa0ab
Andrew Arrow <oneone@gmail.com>
2023-12-25 15:35:14 -0800

    You can't back up the hard drive without calculating the auxiliary XML transmitter!

--- browser/vim_insert.go
+++ browser/vim_insert.go
@@ -6,8 +6,8 @@ func (v *Vim) Insert(k string) {
 	//chars[v.X] = replacementRune
 
 	s := v.Lines[v.Y]
-	prefix := s[0:v.X]
-	suffix := s[v.X:]
+	prefix := s[0 : v.X+1]
+	suffix := s[v.X+1:]
 
 	v.Lines[v.Y] = prefix + k + suffix
 }
