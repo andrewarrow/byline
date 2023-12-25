b8ed0c4582d2cf91d2a5209680eaace5d08aa807
Andrew Arrow <oneone@gmail.com>
2023-12-25 12:36:35 -0800

    Try to synthesize the GB sensor, maybe it will back up the open-source array!

--- browser/vim_render.go
+++ browser/vim_render.go
@@ -1,4 +1,17 @@
 package browser
 
+import (
+	"fmt"
+	"strings"
+)
+
 func (v *Vim) Render() {
+	v.Editor.Set("innerHTML", "")
+	for i, line := range v.Lines {
+		str := fmt.Sprintf(strings.ReplaceAll(line, " ", "&nbsp;"))
+		p := Document.NewTag("p", str)
+		p.Set("id", fmt.Sprintf("line%d", i+1))
+		//p.AddClass("whitespace-nowrap")
+		v.Editor.AppendChild(p.JValue)
+	}
 }
--- markup/vim.mu
+++ markup/vim.mu
@@ -1,6 +1,2 @@
 div text-lg
   div w-full font-mono id=editor
-    p
-      tag id=123 testing
-    p
-      &nbsp;&nbsp;more on this line
