8996290ac256aed62e0bb290148e36df8d2a515a
Andrew Arrow <oneone@gmail.com>
2023-12-25 17:11:18 -0800

    The SSL alarm is down, override the multi-byte system so we can connect the AGP alarm!

--- browser/vim_visual.go
+++ browser/vim_visual.go
@@ -6,6 +6,9 @@ func (v *Vim) VisualArrows(k string) {
 		v.ToY = v.Y
 	} else if k == "y" {
 		v.VisualMode = false
+	} else if k == "d" {
+		v.Lines = append(v.Lines[0:v.FromY], v.Lines[v.ToY+1:]...)
+		v.VisualMode = false
 	} else if k == "ArrowUp" {
 		v.Y--
 		//fmt.Println(v.Y, v.FromY, v.ToY)
