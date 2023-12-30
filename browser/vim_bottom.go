ab792db650f1b9f20c7e925b339c8f2f572b9989
Andrew Arrow <oneone@gmail.com>
2023-12-30 10:27:53 -0800

    I'Ll program the back-end SQL system, that should synthesize the JBOD bus!

--- browser/vim_bottom.go
+++ browser/vim_bottom.go
@@ -9,14 +9,25 @@ import (
 func (v *Vim) BottomKeyPress(k string) {
 
 	if k == "Enter" {
-		m := map[string]any{}
-		h := markup.ToHTMLFromLines(m, vim.SavedLines)
-		vim.Preview.Set("innerHTML", h)
-		go saveLines(strings.Join(vim.SavedLines, "\n"))
-		vim.BottomMode = false
-		v.BottomText = "&nbsp;"
+		v.BottomCommand(v.BottomText[1:])
+		return
 	} else {
 		v.BottomText += k
 	}
 	vim.Bottom.Set("innerHTML", vim.BottomText)
+}
+
+func (v *Vim) BottomCommand(text string) {
+	m := map[string]any{}
+	if text == "w" {
+		h := markup.ToHTMLFromLines(m, vim.SavedLines)
+		vim.Preview.Set("innerHTML", h)
+		go saveLines(strings.Join(vim.SavedLines, "\n"))
+	} else if text == "new" {
+		vim.SavedLines = []string{"div p-3\n  div\n    blank"}
+		h := markup.ToHTMLFromLines(m, vim.SavedLines)
+		vim.Preview.Set("innerHTML", h)
+	}
+	vim.BottomMode = false
+	v.BottomText = "&nbsp;"
 }
