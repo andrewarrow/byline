5643e6ec4ac7bfa0f6763cee28981524bad83080
Andrew Arrow <oneone@gmail.com>
2023-12-30 11:52:52 -0800

    Try to copy the THX matrix, maybe it will override the digital circuit!

--- browser/vim_grow.go
+++ browser/vim_grow.go
@@ -1,11 +1,32 @@
 package browser
 
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+var widthHightRegex = regexp.MustCompile(`/(\d+)/(\d+)`)
+
 func (v *Vim) GrowTag(k string) {
+	line := v.getLine()
+	matches := widthHightRegex.FindStringSubmatch(line)
+	width, _ := strconv.Atoi(matches[1])
+	height, _ := strconv.Atoi(matches[2])
+
 	if k == "ArrowUp" {
+		height += 10
 	} else if k == "ArrowDown" {
+		height -= 10
 	} else if k == "ArrowRight" {
+		width += 10
 	} else if k == "ArrowLeft" {
+		width -= 10
 	}
+	spaces := getSpaces(line)
+	newLine := fmt.Sprintf("%simg src=http://placekitten.com/%d/%d rounded-full",
+		spaces, width, height)
+	v.SavedLines[v.Y+v.FocusStart+v.Offset] = newLine
 	v.Render()
 	leaveInsertMode()
 }
